46734ca2d00e06d6f78a6fb59c9f5a2b1796f4a6
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-07-23 10:25:39 +0300

    added config

---
+++ files/config.php
@@ -0,0 +1,7 @@
+<?php
+  define ('WSITE_ROOT', 'http://localhost/pentastagiu');
+  // DATABASE
+  define ('DB_HOST',  'localhost');
+  define ('DB_NAME',  'test');
+  define ('DB_USER',  'root');
+  define ('DB_PASS',  '');
